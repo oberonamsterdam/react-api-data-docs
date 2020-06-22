5f871596d1dc5c9fc10e60ca87d58d31507f5f5d
Jesse <jesse.vanderheijden@gmail.com>
2020-06-22 10:45:08 +0200

    initial commit

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,15 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/.docz/.cache/dev-404-page.js"))),
+  "component---readme-md": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/README.md"))),
+  "component---src-components-alert-mdx": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/src/components/Alert.mdx"))),
+  "component---src-components-button-mdx": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/src/components/Button.mdx"))),
+  "component---src-index-mdx": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/src/index.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/jessevanderheijden/Documents/projecten/react-api-data-docs/.docz/src/pages/404.js")))
+}
+
