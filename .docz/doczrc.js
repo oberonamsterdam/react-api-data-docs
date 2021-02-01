793f8a39758722b741a48eaeb5dab42a30080294
Gijs Kattenberg <gijs@oberon.nl>
2021-02-01 13:23:02 +0100

    Test config

--- .docz/doczrc.js
+++ .docz/doczrc.js
@@ -1,3 +1,15 @@
 export default {
   typescript: true,
+  themeConfig: {
+    fonts: {
+      body: 'serif',
+    },
+    style: {
+      h2: {
+        color: 'red',
+        fontWeight: 500,
+      },
+    }
+  },
+  description: 'Automate calling external APIs and handling response data for React apps.',
 }
