70737064b8314cd6047501f202e4aac3e0547655
Gijs Kattenberg <gijs@oberon.nl>
2021-07-05 14:36:04 +0200

    Change the description

--- doczrc.js
+++ doczrc.js
@@ -1,15 +1,3 @@
 export default {
-  typescript: true,
-  themeConfig: {
-    fonts: {
-      body: 'serif',
-    },
-    style: {
-      h2: {
-        color: 'red',
-        fontWeight: 500,
-      },
-    }
-  },
-  description: 'Automate calling external APIs and handling response data for React apps.',
+  description: 'Automate calling external APIs and handling response data for React apps.'
 }
