478d5a0c1fb096c19fe66af732872e5cfeea8f26
Gijs Kattenberg <gijs@oberon.nl>
2021-07-05 16:42:26 +0200

    white background

--- doczrc.js
+++ doczrc.js
@@ -1,3 +1,17 @@
 export default {
-  description: 'Automate calling external APIs and handling response data for React apps.'
+  description: 'Automate calling external APIs and handling response data for React apps.',
+  themeConfig: {
+    useScopingInPlayground: true,
+    mode: 'dark',
+    colors: {
+      header: {
+        bg: 'white',
+        border: 'white',
+      },
+      sidebar: {
+        bg: 'white',
+        border: 'white',
+      },
+    },
+  },
 }
